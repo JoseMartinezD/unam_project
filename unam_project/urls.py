fd897395cdf690ff0b2f534dbf9381def3bb6d6f
José <pepetsmart@gmail.com>
2025-05-21 14:39:25 -0600

    API Rest

--- unam_project/urls.py
+++ unam_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("modulos/", include("modulos.urls"))
+    path("modulos/", include("modulos.urls")),
+    path("api/", include("api.urls"))
 ]
